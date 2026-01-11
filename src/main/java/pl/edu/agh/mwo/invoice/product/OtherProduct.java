f306d6a9f17c2b7a9e8a2a55f8dee9af0d8a5e06
JohnnyIronworker <94120112+JohnnyIronworker@users.noreply.github.com>
2026-01-11 13:24:14 +0100

    First product working

--- src/main/java/pl/edu/agh/mwo/invoice/product/OtherProduct.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/OtherProduct.java
@@ -1,9 +1,12 @@
 package pl.edu.agh.mwo.invoice.product;
 
+import javax.lang.model.element.Name;
 import java.math.BigDecimal;
 
 public class OtherProduct extends Product {
     public OtherProduct(String name, BigDecimal price) {
         super(name, price, new BigDecimal("0.23"));
     }
+
+
 }
